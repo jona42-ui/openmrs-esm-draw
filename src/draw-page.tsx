d3c5c703391551f72b51c356e5835de6afca699a
jona42-ui <jonathanthembo123@gmail.com>
2023-07-04 12:18:11 +0300

    align configs

--- src/draw-page.tsx
+++ src/draw-page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DrawingWidget from "./components/drawing-widget/drawing-widget.component";
 
 const DrawPage: React.FC = () => {
   // Add your draw page component implementation here
@@ -7,6 +8,7 @@ const DrawPage: React.FC = () => {
     <div>
       <h1>Draw Page</h1>
       <p>Welcome to the Draw Page!</p>
+      <DrawingWidget selectedImage={""} taskDescription={""} images={[]} />
       {/* Add your draw page content and components here */}
     </div>
   );
