d1fa5f593cb30f22b0f2f2d1fb7a049d683a452a
Dennis Kigen <kigen.work@gmail.com>
2023-07-05 22:49:40 +0300

    (fix) Various fixes to get this frontend module working

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
-import { getAsyncLifecycle } from "@openmrs/esm-framework";
+import { defineConfigSchema, getAsyncLifecycle } from "@openmrs/esm-framework";
+import { configSchema } from "./config-schema";
+
+const moduleName = "@openmrs/esm-draw-app";
 
 const options = {
   featureName: "draw",
-  moduleName: "@openmrs/esm-draw",
+  moduleName: "@openmrs/esm-draw-app",
 };
 
-export const drawPage = {
-  load: getAsyncLifecycle(() => import("./draw-page"), options),
-  route: "draw",
-  order: 2,
-};
+export const importTranslation = require.context(
+  "../translations",
+  false,
+  /.json$/,
+  "lazy"
+);
 
-export const drawExtension = {
-  name: "draw-extension",
-  load: getAsyncLifecycle(() => import("./"), options),
-  slot: "draw-slot",
-};
+export const drawPage = getAsyncLifecycle(
+  () => import("./draw-page.component"),
+  options
+);
 
-export default {
-  pages: [drawPage],
-  extensions: [drawExtension],
-};
+export function startupApp() {
+  defineConfigSchema(moduleName, configSchema);
+}
